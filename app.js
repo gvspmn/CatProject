e5d78e5f27b8b4cc3d4c33250f5822a16c6ff943
Yana Bydliovska <yanabydliovska@gmail.com>
2026-04-01 13:47:31 +0000

    finished monitoring

--- app.js
+++ app.js
@@ -143,5 +143,5 @@ app.get('/metrics', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(\`Котячий сервер запущено на порту \${port}\`);
+    console.log(`Котячий сервер запущено на порту ${port}`);
 });
